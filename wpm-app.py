0a8778bec158ed8b5e0ce102dfb7c7c3942ae97b
satotsuyo <satotsuyo@hirosaki-u.ac.jp>
2025-03-10 19:00:53 +0900

    Update wpm-app.py

--- wpm-app.py
+++ wpm-app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 # タイトルを表示
 st.title("WPM（Words Per Minute）測定アプリ")
@@ -23,3 +24,31 @@ if text:
 
     # 総語数を表示
     st.write(f"これから読む英文の総語数は {totalwords} です")
+
+    # 読む時間を取得
+    if st.button("リーディングスタート"):
+        st.write("読み終わったら、終了ボタンを押してください")
+        start_time = time.time()
+
+    # 終了ボタンで時間を計測
+    if st.button("終了"):
+        end_time = time.time()
+        reading_time_s = end_time - start_time
+        reading_time_m = reading_time_s / 60
+
+        # 読む時間が0だった時、再計測
+        while reading_time_s == 0:
+            st.write("読む時間が0秒です。再度計測してください")
+            if st.button("リーディングスタート（再度）"):
+                start_time = time.time()
+            if st.button("終了（再度）"):
+                end_time = time.time()
+                reading_time_s = end_time - start_time
+                reading_time_m = reading_time_s / 60
+
+        # WPMを計算
+        wpm = totalwords / reading_time_m
+
+        # 結果を表示
+        st.write(f"かかった時間は {round(reading_time_m, 2)} 分で")
+        st.write(f"あなたのWPMは: {round(wpm, 2)} でした")
